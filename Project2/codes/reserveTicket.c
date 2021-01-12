7e3f47430826e13a06d851e9fa85f041abda5dc5
WynChristian <62945850+WynChristian@users.noreply.github.com>
2021-01-12 12:29:10 +0800

    reserve ticket phasseeee!!!

---
+++ Project2/codes/reserveTicket.c
@@ -0,0 +1,168 @@
+#define TOTALMEMBERS (16)
+
+void propmtUserAdultMembers(int *result, int *answer, int *members)
+{
+  *result = 0;
+  *answer = 0;
+  printf("\nHow many adults(18+ years old): ");
+  *result = scanf("%d", answer);
+  while (!(*result) || *answer < 0 || *answer > TOTALMEMBERS)
+  {
+    if (*answer > TOTALMEMBERS)
+    {
+      printf("\nSorry, we can reserve for maximum of %d members only", TOTALMEMBERS);
+      printf("\nHow many adults: ");
+      *result = scanf("%d", answer);
+      break;
+    }
+
+    puts("Please enter valid input\n");
+    if (!(*result))
+    {
+      char removeBuffer[100];
+      scanf("%s", removeBuffer);
+    }
+    printf("\nHow many adults: ");
+    *result = scanf("%d", answer);
+  }
+
+  *members += *answer;
+  return;
+}
+void propmtUserAdultAges(int *result, int *answer, int *members)
+{
+  for (int i = 0; i < *members; i++)
+  {
+    *answer = 0;
+    *result = 0;
+    printf("\nEnter age: ");
+    *result = scanf("%d", answer);
+    while (*answer < 18 || !(*result) || *answer > 150)
+    {
+      puts("Please enter valid input");
+      if (!(*result))
+      {
+        char removeBuffer[100];
+        scanf("%s", removeBuffer);
+      }
+      if (*answer < 18 && *result)
+      {
+        puts("Warning: Age is too low for adult");
+      }
+      printf("\nEnter age: ");
+      *result = scanf("%d", answer);
+    }
+  }
+}
+void propmtUserChildren(int *result, int *answer, int *members)
+{
+  if (*members == TOTALMEMBERS)
+    return;
+  *result = 0;
+  *answer = 0;
+
+  printf("\nHow many children(17- years old): ");
+  *result = scanf("%d", &*answer);
+  int tempTotalMembers = (*members) + *answer;
+  // Data validation
+  while (!(*result) || *answer < 0 || *answer > TOTALMEMBERS || tempTotalMembers > TOTALMEMBERS)
+  {
+    if (tempTotalMembers < TOTALMEMBERS)
+    {
+      printf("\nSorry, we can reserve for maximum of %d members only", TOTALMEMBERS);
+      printf("\nAvailable reservation = %d", TOTALMEMBERS - *members);
+    }
+    if (*answer > TOTALMEMBERS)
+    {
+      printf("\nSorry, we can reserve for maximum of %d members only", TOTALMEMBERS);
+      printf("\nHow many adults: ");
+      *result = scanf("%d", answer);
+      break;
+    }
+    puts("Please enter valid input");
+    if (!(*result))
+    {
+      char removeBuffer[100];
+      scanf("%s", removeBuffer);
+    }
+    printf("\nHow many adults(17- years old): ");
+    *result = scanf("%d", answer);
+  }
+
+  *members += *answer;
+}
+
+void propmtUserChildrenAges(int *result, int *answer, int *members)
+{
+  int totalChild = *answer;
+  for (int i = 0; i < totalChild; i++)
+  {
+    *answer = 0;
+    result = 0;
+    printf("\nEnter age: ");
+    *result = scanf("%d", &*answer);
+    while (*answer > 17 || !(*result) || *answer < 0)
+    {
+      puts("Please enter valid input\n");
+      if (!(*result))
+      {
+        char removeBuffer[100];
+        scanf("%s", removeBuffer);
+      }
+      if (*answer < 18)
+      {
+        puts("Warning: Age is too high for children");
+      }
+      printf("\nEnter age: ");
+      *result = scanf("%d", answer);
+    }
+  }
+}
+
+void reserveTicket(FILE *file, Information (*arrayCategories)[], int *total)
+{
+  //Display destinations-
+  puts("FLIGHT DESTINATION\n");
+  for (int i = 0; i < *total; i++)
+  {
+    printf("%d %-19s %d.00\n", (i + 1), (*arrayCategories)[i].country, (*arrayCategories)[i].price);
+  }
+  printf("%d %s\n", *total + 1, "Return to MAIN");
+
+  //Prompt user (Choose categories)
+  printf("Select reserve destination: ");
+
+  int answer;
+  int result;
+  result = scanf("%d", &answer);
+  while (answer > *total || answer < 0 || !result)
+  {
+    puts("Please enter valid input\n");
+    printf("Select reserve destination: ");
+    result = scanf("%d", &answer);
+
+    if (!result)
+    {
+      char removeBuffer[100];
+      scanf("%s", removeBuffer);
+    }
+  }
+
+  if (answer == *total)
+    return;
+
+  char tempCountry[100];
+  int totalMembers = 0;
+  struct database reservedDATA[20];
+  //Store the current country
+  strcpy(tempCountry, (*arrayCategories)[answer - 1].country);
+
+  //propmt user (Quantity of adult members (18+ years old))
+  propmtUserAdultMembers(&result, &answer, &totalMembers);
+  //Prompt user (adult member's age)
+  propmtUserAdultAges(&result, &answer, &totalMembers);
+  //propmt user (Quantity of children members (17- years old))
+  propmtUserChildren(&result, &answer, &totalMembers);
+  //Prompt user (children member's age)
+  propmtUserChildrenAges(&result, &answer, &totalMembers);
+}
--- Project2/main.c
+++ Project2/main.c
@@ -25,32 +25,42 @@ struct info
   int price;
   float tax;
 };
-#include "codes/manageDestinations.c"
+struct database
+{
+  int age;
+  int price;
+};
 
+#include "codes/manageDestinations.c"
+#include "codes/reserveTicket.c"
 // -------------- MANAGE DESTINATION --------------
 int scanDest(FILE *file, Category *current, int *total);
 void recordDest(Category *category, int index, Information (*categories)[]);
 void readAllDest(FILE *file, Category *category, int *total, Information (*categories)[]);
 
 void addDest(FILE *file, Information (*arrayCategories)[], int *total, bool check);
-
 void editDest(FILE *file, Information (*arrayCategories)[], int total, bool check);
-
 void deleteDest(FILE *file, Information (*arrayCategories)[], int *total, bool check);
-
 void displayDest(Information (*arrayCategories)[], int total);
-
 void manageDestination(FILE *file, Information (*arrayCategories)[], int *total);
+
+// --------------- RESERVE TICKET ------------------
+void propmtUserAdultMembers(int *result, int *answer, int *members);
+void propmtUserAdultAges(int *result, int *answer, int *members);
+void reserveTicket(FILE *file, Information (*arrayCategories)[], int *total);
 
 int main(void)
 {
   FILE *destinationFile;
+  FILE *database;
   Category currentCategory;
   Information allCategories[TOTAL];
   unsigned int totalNumCategories = 0;
 
   readAllDest(destinationFile, &currentCategory, &totalNumCategories, &allCategories);
 
-  manageDestination(destinationFile, &allCategories, &totalNumCategories);
+  //manageDestination(destinationFile, &allCategories, &totalNumCategories);
+  reserveTicket(database, &allCategories, &totalNumCategories);
+
   return 0;
 }
