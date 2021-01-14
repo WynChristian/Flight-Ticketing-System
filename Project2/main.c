c0f7f8c3c7ab84a1956170f200db8966afb83f57
WynChristian <62945850+WynChristian@users.noreply.github.com>
2021-01-15 06:39:58 +0800

    add comments in main.c

--- Project2/main.c
+++ Project2/main.c
@@ -6,40 +6,48 @@
 #include <string.h>
 #include <assert.h>
 
+// This is the file path of our source code
 #define destinationsFile "assets/destination.txt"
 #define transactionsFile "assets/historyTransaction.txt"
 #define reportFile "assets/salesReport.txt"
 #define databaseRootFile "database/"
 
-#define TOTAL (100)
+#define TOTAL (100)         // size of an arrays
 #define TOTALMEMBERS (16)   //For reservation per reservation
 #define MAXTRANSACTION (16) //For non reservation per transaction
 
 // This is our external files
-#include "codes/structs.h"
-#include "codes/manageDestinations.c"
-#include "codes/reserveTicket.c"
-#include "codes/buyTicket.c"
-#include "codes/displayReport.c"
-#include "codes/mainMenu.c"
+#include "codes/structs.h"            // just a collection of structs
+#include "codes/manageDestinations.c" // 1.] Manage Destination
+#include "codes/reserveTicket.c"      // 2.] Reserve Ticket
+#include "codes/buyTicket.c"          // 3.] Buy Ticket
+#include "codes/displayReport.c"      // 4.] Display Report
+#include "codes/mainMenu.c"           // The MAIN menu
 
 int main(void)
 {
   Documents Files;
-  Information allCategories[TOTAL];
-  Report salesReports[TOTAL];
-  unsigned int totalNumCategories = 0;
+  Information allCategories[TOTAL]; // to track the current data in `destinationsFile`
+  Report salesReports[TOTAL];       // To track the sales report
+
+  // To track the numbers of categories in the `destinationFile`
+  unsigned int numCategories = 0;
+  // To track the total country in the `salesReport` array
   unsigned int totalCountry = 0;
 
-  readAllDest(Files.getDestinations, &totalNumCategories, &allCategories);
-  initialize(&allCategories, &salesReports, &totalNumCategories,
+  // Get the information in the `destinationFile`
+  readAllDest(Files.getDestinations, &numCategories, &allCategories);
+
+  // Initialize the values in `salesReport` array with 0
+  initialize(&allCategories, &salesReports, &numCategories,
              &totalCountry, transactionsFile);
 
+  // Start the main program
   displayMainMenu(&Files, &allCategories, &salesReports,
-                  &totalNumCategories, TOTALMEMBERS,
+                  &numCategories, TOTALMEMBERS,
                   databaseRootFile, &totalCountry,
                   transactionsFile, reportFile,
                   MAXTRANSACTION);
 
   return 0;
-}
+} // main
