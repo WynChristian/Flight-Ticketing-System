da7b21d53dca6faa7ad018b4a59b241f615df0b5
WynChristian <62945850+WynChristian@users.noreply.github.com>
2021-01-22 11:11:13 +0800

    new: create file for UI designs

---
+++ Project2/codes/designs.c
@@ -0,0 +1,8 @@
+/* 
+  Yo mga group-mates, dito niyo nalang ilagay ang 
+    mga style lines functions niyo para maiwasan
+    ang disorganized codes natin hahhaha
+
+
+    goodluck sainyooo   -KUYA WYYNNN
+ */
